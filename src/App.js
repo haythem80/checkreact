42607633c3112a17eb850c3150d2aa1a5bc88c5f
haythem80 <haythemmlik022@gmail.com>
2024-02-20 23:35:45 +0100

    check

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Navebare from './components/Navebare';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from './components/Card';
+
+function App() {
+  return (
+    <div className='App'>
+      <Navebare />
+      <h1 className= 'text'> welcome to my react checkpoint </h1>
+      <Card />
+      <Card />
+      <Card />
+      </div>
+  )
+}
+
+export default App;
